28e82bb1ee61290c738b56124ba32ba266d2876a
Zamiz <zamiztobar@gmail.com>
2019-01-21 16:20:39 -0300

    style(layout): add bootstrap to app layout style(view): add form_tag with add_tag method to tablefeat(route): add 'add_tag' route to movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :tags
-  resources :movies
+  resources :movies do
+    member do
+      post 'add_tag'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
